ab69e69d640d90b17ec4696eb74122b3b3802f2b
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-06-10 22:27:41 +0200

    Added adaptive topbar icons.

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -14,12 +14,12 @@ export const Topbar: React.FC <{
             <div id="buttons">
                 <a href="https://github.com/andrews-quest" target="_blank">
                     <div id="button">
-                        <img src={gitHubLogo}></img> GitHub
+                        <img src={gitHubLogo}></img> <h2>GitHub</h2>
                     </div>
                 </a>
                 <a href="https://www.linkedin.com/in/andrii-roshkovan/" target="_blank">
                     <div id="button">
-                        <img src={linkedInLogo}></img> LinkedIn 
+                        <img src={linkedInLogo}></img> <h2>LinkedIn</h2> 
                     </div>
                 </a>
             </div>
