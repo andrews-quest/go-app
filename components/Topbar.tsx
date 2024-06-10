2b519d07aaf32299dd3fadf18f3b4d5d2d094f97
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-06-10 23:26:37 +0200

    Added transparency to topbar buttons and removed link styling.

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -33,12 +33,12 @@ export const Topbar: React.FC <{
             <div id="buttons">
                 <a href="https://github.com/andrews-quest" target="_blank">
                     <div id="button">
-                        <img src={gitHubLogo}></img> <h2>GitHub</h2>
+                        <img src={gitHubLogo}></img> <h2> GitHub</h2>
                     </div>
                 </a>
                 <a href="https://www.linkedin.com/in/andrii-roshkovan/" target="_blank">
                     <div id="button">
-                        <img src={linkedInLogo}></img> <h2>LinkedIn</h2> 
+                        <img src={linkedInLogo}></img> <h2> LinkedIn</h2> 
                     </div>
                 </a>
             </div>
